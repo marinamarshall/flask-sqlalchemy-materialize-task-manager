8288d7e9bf7e7e4764881ec80b03a2305938b3c4
marinamarshall <marinamarshall2@gmail.com>
2022-03-15 18:18:31 +0000

    Creating the Database

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,6 +2,9 @@ from taskmanager import db
 
 
 class Category(db.Model):
+    """
+    category class
+    """
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
@@ -13,6 +16,9 @@ class Category(db.Model):
 
 
 class Task(db.Model):
+    """
+    task class
+    """
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
